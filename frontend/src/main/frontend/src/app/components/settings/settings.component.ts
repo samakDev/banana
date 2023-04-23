bccd41efbdfeb67e0c67c9eaca3ad3398739b278
samak <samak.developer@gmail.com>
2023-04-23 11:35:14 +0200

    clean: improve GUI settings readability

--- frontend/src/main/frontend/src/app/components/settings/settings.component.ts
+++ frontend/src/main/frontend/src/app/components/settings/settings.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ContextService } from '../../services/context.service';
-import { TranslateService } from '@ngx-translate/core';
+import {Component, OnInit} from '@angular/core';
+import {ContextService} from '../../services/context.service';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-settings',
@@ -13,13 +13,16 @@ export class SettingsComponent implements OnInit {
   isShowWeekend: Boolean = false;
   currentLang: string;
   langs: Array<string>;
+
   constructor(private contextService: ContextService, private translateService: TranslateService) {
-    this.contextService.getFullScreenMode()//
+    this.contextService.getFullScreenMode()
       .subscribe(v => this.isFullScreen = v, e => console.error(e));
-    this.contextService.getShowWeekend()//
+
+    this.contextService.getShowWeekend()
       .subscribe(v => this.isShowWeekend = v, e => console.error(e));
-    this.currentLang = this.translateService.currentLang,
-      this.langs = this.translateService.getLangs();
+
+    this.currentLang = this.translateService.currentLang;
+    this.langs = this.translateService.getLangs();
 
   }
 
@@ -29,9 +32,11 @@ export class SettingsComponent implements OnInit {
   onFullScreenChange(checked: Boolean) {
     this.contextService.setFullScreenMode(checked);
   }
+
   onShowWeekendChange(show: Boolean) {
     this.contextService.setShowWeekend(show);
   }
+
   onCurrentLangChange(currentLang: string) {
     this.translateService.use(currentLang);
   }
--- frontend/src/main/frontend/src/app/services/context.service.ts
+++ frontend/src/main/frontend/src/app/services/context.service.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
 import {BehaviorSubject, Observable, Subject} from 'rxjs/Rx';
 import {LocalStorageService} from 'angular-2-local-storage';
-import {NavigationEnd, Router} from '@angular/router';
 import {Hotkey, HotkeysService} from 'angular2-hotkeys';
 
 @Injectable()
@@ -10,32 +9,17 @@ export class ContextService {
   private fullScreenMode: Subject<Boolean> = new BehaviorSubject(false);
   private showWeekend: Subject<Boolean> = new BehaviorSubject(false);
   private isFullScreen: Boolean = false;
-  private viewSelected: Observable<string>;
 
   constructor(private localStorageService: LocalStorageService,
-              private router: Router,
               private hotkeysService: HotkeysService) {
-    this.viewSelected = router.events
-      .filter(evt => evt instanceof NavigationEnd)
-      .map((evt: NavigationEnd) => evt.url)
-      .map(url => url.substring(1))
-      .map(url => {
-        const index = url.indexOf('/');
-        if (index !== -1) {
-          return url.substring(0, index);
-        }
-        return url;
-      }).shareReplay(1);
     this.setFullScreenMode(this.localStorageService.get<Boolean>('isFullScreen'));
+
     this.hotkeysService.add(new Hotkey('ctrl+shift+f', evt => {
       this.setFullScreenMode(!this.isFullScreen);
       return false;
     }));
-    this.setShowWeekend(this.localStorageService.get<Boolean>('showWeekend'));
-  }
 
-  public getViewSelected(): Observable<string> {
-    return this.viewSelected;
+    this.setShowWeekend(this.localStorageService.get<Boolean>('showWeekend'));
   }
 
   public getFullScreenMode(): Observable<Boolean> {
@@ -56,6 +40,4 @@ export class ContextService {
     this.localStorageService.set('showWeekend', show);
     this.showWeekend.next(show);
   }
-
-
 }
