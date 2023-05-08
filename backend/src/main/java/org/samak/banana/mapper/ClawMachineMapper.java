6fb92c96a53ffe7fa250c966640975cef797df4a
samak <samak.developer@gmail.com>
2023-05-08 14:24:37 +0200

    backend - refacto: add nullcheck on ClawMachineMapper

--- backend/src/main/java/org/samak/banana/mapper/ClawMachineMapper.java
+++ backend/src/main/java/org/samak/banana/mapper/ClawMachineMapper.java
@@ -4,10 +4,16 @@ import org.mapstruct.Mapper;
 import org.samak.banana.dto.model.ClawMachine;
 import org.samak.banana.entity.ClawMachineEntity;
 
+import java.util.Objects;
+
 @Mapper
 public interface ClawMachineMapper {
 
     default ClawMachine convertClawMachineEntityToDto(ClawMachineEntity clawMachineEntity) {
+        if (Objects.isNull(clawMachineEntity) || Objects.isNull(clawMachineEntity.getId())) {
+            return null;
+        }
+
         return ClawMachine.newBuilder()
                 .setId(clawMachineEntity.getId().toString())
                 .setName(clawMachineEntity.getName())
